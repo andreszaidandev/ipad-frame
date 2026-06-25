190af4c141b608a6745f8e3db15a497f69414be5
andreszaidandev <andreszaidan02@gmail.com>
2026-06-25 01:57:19 -0400

    callbackfixe

--- spotify-poster/src/pages/Callback.tsx
+++ spotify-poster/src/pages/Callback.tsx
@@ -1,11 +1,24 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { spotify } from "../spotify";
 
 export default function Callback() {
+  const navigate = useNavigate();
+
   useEffect(() => {
-    spotify.authenticate().then(() => {
-      window.location.href = "/";
-    });
+    async function run() {
+      try {
+        await spotify.authenticate();
+
+        // IMPORTANT: go to protected page, not login
+        navigate("/poster");
+      } catch (err) {
+        console.error("Auth failed", err);
+        navigate("/");
+      }
+    }
+
+    run();
   }, []);
 
   return <div>Authorizing...</div>;
